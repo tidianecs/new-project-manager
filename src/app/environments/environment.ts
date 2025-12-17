f720f6e715969876a534474c0dd1910e395350c6
Tidiane <tidianecs20pgmail.com>
2025-12-17 14:04:50 +0000

    Test dor the deployment 2

---
+++ src/app/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  supabaseUrl: 'https://nizqpfbxaprdgqgweazv.supabase.co',
+  supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5penFwZmJ4YXByZGdxZ3dlYXp2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjUxOTczODgsImV4cCI6MjA4MDc3MzM4OH0.noRT1JqhRL0l08nSGW50pa6G6orWkelVXIKfhyMDw0U ',
+};
