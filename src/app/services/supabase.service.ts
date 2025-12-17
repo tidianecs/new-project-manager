79b961f5d7485d9e64a8cebfbc7e69cb07327f3b
Tidiane <tidianecs20pgmail.com>
2025-12-17 16:53:14 +0000

    try to fix: use @supabase/supabase-js instead of supabase prt 2

--- src/app/services/supabase.service.ts
+++ src/app/services/supabase.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { createClient, SupabaseClient } from '@supabase/supabase-js';
+import { createClient } from '@supabase/supabase-js';
 import { environment } from '../environments/environment';
 
 @Injectable({
